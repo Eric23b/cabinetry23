b2cd31b5bd0b588bda5df847d2d25d2b09255ebb
Eric <eric23b@icloud.com>
2023-05-10 16:27:27 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,93 @@
+<script>
+  import ProjectsTab from './lib/ProjectsTab.svelte'
+	import { onMount } from 'svelte';
+
+  let tabBtns = {}
+  let currentTabIndex = 0;
+  onMount(async () => {
+    // tabBtns.projects.style.color = 'var(--button-text)';
+	});
+
+  /**
+     * @param {string} tab
+  */
+  function showTab(tab) {
+    greyTabBtns();
+    tabBtns[tab].style.color = 'var(--highlight-text)';
+    tabBtns[tab].style.backgroundColor = 'var(--highlight)';
+    currentTabIndex = Object.keys(tabBtns).indexOf(tab);
+  }
+
+  function greyTabBtns() {
+    for (const key in tabBtns) {
+      tabBtns[key].style.color = 'var(--gray-text)';
+      tabBtns[key].style.backgroundColor = 'var(--canvas)';
+    }
+  }
+
+</script>
+
+<main>
+  <nav>
+    <button bind:this={tabBtns.projects} on:click={() => {showTab('projects')}} class="start-color">Projects</button>
+    <div class="flex-spacer"></div>
+    <button bind:this={tabBtns.projectSummary} on:click={() => {showTab('projectSummary')}}>Project Summary</button>
+    <button bind:this={tabBtns.designer} on:click={() => {showTab('designer')}}>Designer</button>
+    <button bind:this={tabBtns.additionalItems} on:click={() => {showTab('additionalItems')}}>Additional Items</button>
+    <button bind:this={tabBtns.cutlist} on:click={() => {showTab('cutlist')}}>Cutlist</button>
+    <button bind:this={tabBtns.doorList} on:click={() => {showTab('doorList')}}>Door List</button>
+    <button bind:this={tabBtns.estimate} on:click={() => {showTab('estimate')}}>Estimate Breakdown</button>
+    <button bind:this={tabBtns.invoice} on:click={() => {showTab('invoice')}}>Invoice</button>
+    <div class="flex-spacer"></div>
+    <button bind:this={tabBtns.settings} on:click={() => {showTab('settings')}}>Settings</button>
+  </nav>
+  <section>
+    {#if currentTabIndex == 0}
+      <ProjectsTab/>
+    {:else if currentTabIndex == 1}
+      <ProjectsTab/>
+    {/if}
+  </section>
+  <footer>Status</footer>
+</main>
+
+<style>
+  main{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  nav{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    border-bottom: 1px solid var(--button-border);
+  }
+  nav button{
+    font-size: 1rem;
+    margin: 0.1rem 0;
+    padding: 0.25rem 0.5rem;
+    border: 1px solid var(--button-border);
+    border-radius: 0.5rem 0.5rem 0 0;
+    border-bottom: 0;
+    color: var(--gray-text);
+    background-color: var(--canvas);
+  }
+  /* Active tab on app start */
+  .start-color{
+    color: var(--highlight-text);
+    background-color: var(--highlight);
+  }
+
+  main > section{
+    flex-grow: 1;
+    position: relative;
+  }
+
+  footer{
+    padding: 0.25rem 0.5rem;
+    font-size: 1.2rem;
+    border-top: 1px solid var(--button-border);
+  }
+</style>
---
+++ src/lib/ProjectsTab.svelte
@@ -0,0 +1,77 @@
+<section>
+    <table>
+        <tr>
+            <th>ID#</th>
+            <th>Customer</th>
+            <th>Start Date</th>
+            <th>Delivery Date</th>
+            <th>Install Date</th>
+            <th>Paid</th>
+            <th>Completed</th>
+        </tr>
+        <tr>
+            <td>1.a</td>
+            <td>Bill</td>
+            <td>2023-08-01</td>
+            <td>2023-08-10</td>
+            <td>2023-08-15</td>
+            <td>✓</td>
+            <td>✓</td>
+        </tr>
+        <tr>
+            <td>1.a</td>
+            <td>Bill</td>
+            <td>2023-08-01</td>
+            <td>2023-08-10</td>
+            <td>2023-08-15</td>
+            <td>✓</td>
+            <td>✓</td>
+        </tr>
+        <tr>
+            <td>1.a</td>
+            <td>Bill</td>
+            <td>2023-08-01</td>
+            <td>2023-08-10</td>
+            <td>2023-08-15</td>
+            <td>✓</td>
+            <td>✓</td>
+        </tr>
+    </table>
+</section>
+
+<style>
+    section{
+        position: absolute;
+        inset: 0;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    table{
+        border: 1px solid var(--button-border);
+        border-spacing: 0;
+    }
+    tr:nth-child(even){
+        background-color: var(--button-face);
+    }
+    tr:hover{
+        background-color: var(--highlight);
+    }
+    tr:first-child{
+        background-color: var(--canvas);
+        border-bottom: 1px solid var(--button-border);
+    }
+    th{
+        padding: 0.5rem 1rem;
+        text-align: center;
+        background: transparent;
+    }
+    td{
+        text-align: center;
+        background: transparent;
+        cursor: pointer;
+    }
+</style>
